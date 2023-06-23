e52826e72898c281e2904426ea8d44bffb66f050
Karen Banegas <Karenbanegas2002@unitec.edu>
2023-06-22 23:27:42 -0600

    react_router

---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter  } from 'react-router-dom';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
+import Contact, { loader as contactLoader, action as contactAction } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from "./routes/destroy";
+import ErrorPage from './error_page';
+import Index from './routes/index';
+import { RouterProvider } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+      ],
+    },
+  ],
+},
+]);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
